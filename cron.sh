82a13cf41a4d566dfdde7d86d750ec23bd4ad1a8
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2022-06-23 20:03:47 +0100

    My first social credit

---
+++ cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-setup-1.11.1-SNAPSHOT-javadoc.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-setup-1.11.1-SNAPSHOT-sources.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-setup.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-tools-1.11.1-SNAPSHOT-javadoc.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-tools-1.11.1-SNAPSHOT-sources.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/gdx-tools-1.11.1-SNAPSHOT.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-nightlies/natives.zip
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/runnable-2D-particles.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/runnable-3D-particles.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/runnable-hiero.jar
+wget -N https://libgdx-nightlies.s3.eu-central-1.amazonaws.com/libgdx-runnables/runnable-texturepacker.jar
